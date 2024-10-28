e4c4e7099453afeea0b163a6e7d4d2db28c6b362
NicolasYapobi <nicolas.yapobi@epitech.eu>
2024-10-28 12:20:14 +0100

    update: remove background image

--- app/pages/index.js
+++ app/pages/index.js
@@ -4,12 +4,10 @@ import Convert from './convert';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div className={styles.overlay}>
-        <div className={styles.titlePanel}>
-          <h1 className={styles.title}>Convertisseur de monnaie</h1>
-          <p className={styles.subtitle}>Korean Convertor</p>
-        </div>
-      <Convert/>
+      <div className={styles.titlePanel}>
+        <h1 className={styles.title}>Convertisseur de monnaie</h1>
+        <p className={styles.subtitle}>Korean Convertor</p>
+        <Convert/>
       </div>
     </div>
   );
