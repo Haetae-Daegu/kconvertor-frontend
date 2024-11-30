464a7510189673a8cd976dc3555cef9f60659b80
NicolasYapobi <nicolas.yapobi@epitech.eu>
2024-11-30 22:16:56 +0100

    update: call API added

--- src/pages/convert.jsx
+++ src/pages/convert.jsx
@@ -1,50 +1,5 @@
 import {useEffect, useState} from "react"
-import styles from '../styles/Convert.module.css';
-
-// export default function Convert() {
-
-//     const [currency, setCurrency] = useState(0);
-    
-//     const handleInput = (e) => {
-//       const fieldName = e.target.name;
-//       const fieldValue = e.target.value;
-
-//       setCurrency((prevState) => ({
-//         ...prevState,
-//         [fieldName]: fieldValue
-//       }));
-//     };
-
-
-//     function submitForm() {
-//       console.log("tyoto")
-//       useEffect(() => {
-//         fetch('/currency/')
-//           .then(res => res.json())
-//           .then(data => {
-//             console.log(data) 
-//             setMessage(data.message);
-//             setLoading(false);
-//           })
-//       }, [])
-//     }
-
-//     return (
-//       <div className={styles.convertContainer}>
-//         <div className={styles.convertBox}>
-//           <form method="POST" >
-//             <div>
-//                 <label>Euros</label>
-//                 <input type="number" name="euros" onChange={handleInput}/>
-//             </div>
-//             <h2>WONS</h2>
-//             <button className={styles.convertButton} onChange={submitForm}>Convert</button>
-//           </form>           
-//         </div>
-//       </div>
-//     );
-// }
-  
+import axios from 'axios';
 
 const Convert = () => {
   const [amount, setAmount] = useState("");
@@ -54,6 +9,12 @@ const Convert = () => {
 
   const handleConvert = () => {
     const conversionRate = 0.85;
+    axios.get('http://127.0.0.1:5000/currency/', {})
+      .then(res => {
+        const from = res.data["KRW"]
+        setResult(from);
+
+      })
     setResult((amount * conversionRate).toFixed(2));
   };
 
