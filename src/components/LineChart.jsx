f508c156be6646e1d065b82f7a2497a124d2bcaf
NicolasYapobi <nicolas.yapobi@epitech.eu>
2025-01-06 21:28:59 +0100

    feat - graph: add size ratio

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,21 +1,25 @@
 import { Line } from "react-chartjs-2";
 
 const LineChart = ({ chartData }) => (
-  <div>
+  <div className="mx-auto max-w-3xl flex flex-col gap-6 rounded-3xl bg-white p-6 shadow-3xl md:p-8 mt-6">
     <Line
-      data={chartData}
-      options={{
-        plugins: {
-          title: {
-            display: true,
-            text: "mockData"
+        data={chartData}
+        height="200px"
+        width="200px"
+        options={{
+          plugins: {
+            title: {
+              display: true,
+              text: "mockData"
+            },
+            legend: {
+              display: false
+            }
           },
-          legend: {
-            display: false
-          }
-        }
-      }}
-    />
+          aspectRatio: 1,
+          maintainAspectRatio: false
+        }}
+      />
   </div>
 );
 
