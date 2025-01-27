87ba33f6f0080f291241fc2bbc02910d380cda06
NicolasYapobi <nicolas.yapobi@epitech.eu>
2025-01-28 00:25:59 +0100

    fix: update overflow add button toggle component

--- src/pages/chart/chart.jsx
+++ src/pages/chart/chart.jsx
@@ -2,14 +2,15 @@ import Chart from "chart.js/auto";
 import LineChart from "../../components/LineChart";
 import { useGraphData } from "../../hooks/useGraphData";
 import ErrorPanel from "../../components/ErrorPanel";
+import ToggleButton from "../../components/ToggleButton";
 
 import { CategoryScale } from "chart.js"; 
 import { useEffect, useState } from "react";
 
-Chart.register(CategoryScale)
+Chart.register(CategoryScale);
 
 const CurrencyChart = () => {
-  const { handleData, graphData, error} = useGraphData();
+  const { handleData, graphData, error } = useGraphData();
 
   const [isVisible, setIsVisible] = useState(false);
   const [chartData, setChartData] = useState(null);
@@ -26,7 +27,6 @@ const CurrencyChart = () => {
     const [day, month, year] = dateString.split("/").map(Number);
     return new Date(year, month - 1, day);
   };
-  
 
   const filterDataByTimeFrame = () => {
     const now = new Date();
@@ -39,14 +39,13 @@ const CurrencyChart = () => {
       const itemDate = parseDate(item.date);
       return itemDate >= limitDate;
     });
-  }
+  };
 
   useEffect(() => {
     if (graphData.length > 0) {
       const filterData = filterDataByTimeFrame();
-      filterData.map((data) => data.currency_value)
       setChartData({
-        labels: filterData.map((data) => data.day), 
+        labels: filterData.map((data) => data.day),
         datasets: [
           {
             data: filterData.map((data) => data.currency_value),
@@ -55,61 +54,54 @@ const CurrencyChart = () => {
               "#ecf0f1",
               "#50AF95",
               "#f3ba2f",
-              "#2a71d0"
+              "#2a71d0",
             ],
             fill: false,
             borderColor: "black",
-            borderWidth: 1
-          }
-        ]
-      })
+            borderWidth: 1,
+          },
+        ],
+      });
     }
   }, [graphData, timeFrame]);
 
-  function visibility() {
-    setIsVisible((isVisible) => !isVisible);
-  }
+  const toggleVisibility = () => {
+    setIsVisible((prev) => !prev);
+    handleData();
+  };
 
   return (
     <div className="mx-auto max-w-3xl flex flex-col gap-6 rounded-3xl bg-white p-6 shadow-3xl md:p-8 mt-6">
-      <div className="flex gap-4">
-        <h1 className="text-xl font-bold text-gray-800">
-          Graph Currency
-        </h1>
-        <button
-          className="ml-auto rounded-lg bg-blue-500 p-2 hover:bg-gray-300 m6"
-          onClick={() => {visibility(), handleData()}}
-        >
-          {isVisible ? "-" : "+"}
-        </button>
+      <div className="flex flex items-center justify-between">
+        <h1 className="text-xl font-bold text-gray-800">Graph Currency</h1>
+        <ToggleButton isVisible={isVisible} onToggle={toggleVisibility} />
       </div>
 
       {isVisible && (
-        <div className="flex gap-4 mb-4">
-          {Object.keys(timeFrameMap).map((range) => (
-            <button
-              key={range}
-              className={`px-4 py-2 rounded-lg ${
-                timeFrame === range ? "bg-blue-500 text-white" : "bg-gray-200"
-              }`}
-              onClick={() => setTimeFrame(range)}
-            >
-              {range}
-            </button>
-          ))}
-        </div>
-      )}
-
-      {isVisible && chartData ? (
-        <LineChart chartData={chartData} />
-        
-      ) : (
-        isVisible && <p>Loading ...</p>
+        <>
+          <div className="flex gap-4 mb-4">
+            {Object.keys(timeFrameMap).map((range) => (
+              <button
+                key={range}
+                className={`px-4 py-2 rounded-lg ${
+                  timeFrame === range ? "bg-blue-500 text-white" : "bg-gray-200"
+                }`}
+                onClick={() => setTimeFrame(range)}
+              >
+                {range}
+              </button>
+            ))}
+          </div>
+          {chartData ? (
+            <LineChart chartData={chartData} />
+          ) : (
+            <p>Loading...</p>
+          )}
+        </>
       )}
       {error && <ErrorPanel message={error} />}
-
     </div>
   );
-}
+};
 
 export default CurrencyChart;
--- src/pages/currency/convert.jsx
+++ src/pages/currency/convert.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import { useCurrencyConvertor } from "../../hooks/useCurrencyConverter";
 import InputField from "../../components/InputField";
 import CurrencySelector from "../../components/CurrencySelector";
 import ResultDisplay from "../../components/ResultDisplay";
 import ErrorPanel from "../../components/ErrorPanel";
+import ToggleButton from "../../components/ToggleButton";
 
 const Convert = () => {
   const {
@@ -18,43 +20,55 @@ const Convert = () => {
     swapCurrencies,
   } = useCurrencyConvertor();
 
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setIsVisible((prev) => !prev);
+  };
+
   return (
     <div className="mx-auto max-w-3xl flex flex-col gap-6 rounded-3xl bg-white p-6 shadow-3xl md:p-8">
-      <h2 className="text-xl font-bold text-gray-800">
-        Select your currencies and put the amount that you want to convert
-      </h2>
-      <div className="flex flex-col gap-4">
-        <InputField
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-          placeholder="Enter amount"
-        />
-
-        <div className="flex gap-4 items-center">
-          <CurrencySelector
-            value={fromCurrency}
-            onChange={(e) => setFromCurrency(e.target.value)}
-            options={["EUR", "KRW"]}
-          />
-          <button
-            onClick={swapCurrencies}
-            className="rounded-lg bg-blue-500 p-2 hover:bg-gray-300"
-          >
-            ⇆
-          </button>
-          <CurrencySelector
-            value={toCurrency}
-            onChange={(e) => setToCurrency(e.target.value)}
-            options={["EUR", "KRW"]}
-          />
-        </div>
-        <button
-          className="w-full rounded-lg bg-blue-500 p-3 text-white hover:bg-blue-600"
-          onClick={handleConvert}
-        >
-          Convert
-        </button>
+      <div className="flex items-center justify-between">
+        <h2 className="text-xl font-bold text-gray-800">
+          Currency Convertor
+        </h2>
+        <ToggleButton isVisible={isVisible} onToggle={toggleVisibility} />
       </div>
+
+      {isVisible && (
+        <div className="flex flex-col gap-4">
+          <InputField
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Enter amount"
+          />
+
+          <div className="flex gap-4 items-center">
+            <CurrencySelector
+              value={fromCurrency}
+              onChange={(e) => setFromCurrency(e.target.value)}
+              options={["EUR", "KRW"]}
+            />
+            <button
+              onClick={swapCurrencies}
+              className="rounded-lg bg-blue-500 p-2 hover:bg-gray-300"
+            >
+              ⇆
+            </button>
+            <CurrencySelector
+              value={toCurrency}
+              onChange={(e) => setToCurrency(e.target.value)}
+              options={["EUR", "KRW"]}
+            />
+          </div>
+          <button
+            className="rounded-lg bg-blue-500 px-6 py-3 text-white hover:bg-blue-600"
+            onClick={handleConvert}
+          >
+            Convert
+          </button>
+        </div>
+      )}
 
       {result !== null && error === null && (
         <ResultDisplay
