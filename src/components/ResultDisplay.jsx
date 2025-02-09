de48d08f37ff0ab6b8bce3921e8972c53682901b
NicolasYapobi <nicolas.yapobi@epitech.eu>
2025-02-09 19:31:06 +0100

    feat - refacto-ts: ResultDisplay typed

--- src/components/ResultDisplay.jsx
+++
@@ -1,9 +0,0 @@
-const ResultDisplay = ({amount, fromCurrency, result, toCurrency}) => (
-  <div className="mt-4 rounded-lg bg-gray-100 p-4 text-center">
-    <p className="text-lg font-medium text-gray-800">
-      {amount} {fromCurrency} = {result} {toCurrency}
-    </p>
-  </div>
-);
-
-export default ResultDisplay;
---
+++ src/components/ResultDisplay.tsx
@@ -0,0 +1,16 @@
+interface Props {
+  amount: string;
+  fromCurrency: string;
+  result: string;
+  toCurrency: string
+}
+
+const ResultDisplay = (props: Props) => (
+  <div className="mt-4 rounded-lg bg-gray-100 p-4 text-center">
+    <p className="text-lg font-medium text-gray-800">
+      {props.amount} {props.fromCurrency} = {props.result} {props.toCurrency}
+    </p>
+  </div>
+);
+
+export default ResultDisplay;
