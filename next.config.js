2b60866fdef2f2bf25d19a78b85228245ac68782
NicolasYapobi <nicolas.yapobi@epitech.eu>
2025-03-17 12:12:05 +0100

    feat: add Next.js configuration for API rewrites and remote image handling

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://127.0.0.1:5000/:path*',
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'accommodations.lon1.cdn.digitaloceanspaces.com',
+      },
+      {
+        protocol: "https",
+        hostname: "accommodations.lon1.digitaloceanspaces.com",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig; 
