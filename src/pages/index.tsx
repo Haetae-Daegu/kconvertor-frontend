da228aff8ca8668bfbf0ee17e2f024564f698526
NicolasYapobi <nicolas.yapobi@epitech.eu>
2025-04-30 01:11:57 +0200

    feat: add email contact link to the landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -167,6 +167,14 @@ const LandingPage = () => {
                 </svg>
                 <span>Arneol</span>
               </div>
+              <div className="flex items-center">
+                <svg className="w-6 h-6 text-blue-600 mr-2" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
+                </svg>
+                <a href="mailto:nicolas.yapobi@epitech.eu" className="hover:text-blue-600 transition-colors">
+                  nicolas.yapobi@epitech.eu
+                </a>
+              </div>
             </div>
           </div>
         </div>
